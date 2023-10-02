5646dcf2ca2bb242b1bbe6f253ba3e4f12d76d50
danieluau <daniellealuau@gmail.com>
2023-10-02 17:54:10 -0300

    consertei

--- rede-social/client/rede/src/app/(authenticatedPages)/main/page.tsx
+++ rede-social/client/rede/src/app/(authenticatedPages)/main/page.tsx
@@ -1,11 +1,41 @@
+"use client"
+
 import Feed from "@/app/components/Feed";
 import Share from "@/app/components/Share";
+import { useQuery } from "@tanstack/react-query";
+import { makeRequest } from "../../../../axios";
+
+interface IPost {
+    id: number;
+    post_desc: string;
+    img: string;
+    username: string;
+    userImg: string;
+    created_at: string;
+}
+
+
 
 function Main() {
+
+    const postQuery = useQuery<IPost[] | undefined>({
+        queryKey: ['posts'],
+        queryFn: () => makeRequest.get('post/?id=')
+            .then((res) => {
+                return res.data.data;
+            })
+    });
+
+    if (postQuery.error) {
+        console.log(postQuery.error);
+    }
+
+
+
     return (
     <div className="w-2/6 flex flex-col gap-5">
         <Share />
-        <Feed/>
+        <Feed post={postQuery.data}/>
     </div>)
 }
 
