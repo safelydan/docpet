6b393307186dd3761f3c640e150093fdeb24c053
danieluau <daniellealuau@gmail.com>
2023-10-26 20:45:16 -0300

    busca parcialmente feita

--- rede-social/client/rede/src/app/components/Header.tsx
+++ rede-social/client/rede/src/app/components/Header.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueries, useQuery } from "@tanstack/react-query";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useContext, useState } from "react";
@@ -7,12 +7,14 @@ import {FaSearch, FaBell} from 'react-icons/fa'
 import {TbMessageCircle2Filled} from 'react-icons/tb'
 import { makeRequest } from "../../../axios";
 import { UserContext } from "@/context/UserContext";
+import { IUser } from "@/interfaces";
 
 
 function Header() {
 
     const {user, setUser} = useContext(UserContext)
     const [showMenu, setShowMenu] = useState(false);
+    const [search, setSearch ] = useState<string|null>(null)
     const router = useRouter();
 
     const mutation = useMutation({
@@ -29,13 +31,42 @@ function Header() {
     })
 
 
+    const {data, error} = useQuery({
+        queryKey: ['search'],
+        queryFn: ()=> makeRequest.get(`search/search-users?params=${search}`).then((res)=>{
+            return res.data
+        }),
+        enabled: !!search //so vai fazer a req quando houver um user
+    })
+
+    if(error){
+        console.log(error)
+    }
+
+    console.log(data)
 
     return(
         <header className=" fixed z-10 w-full bg-white flex justify-between py-2 px-4 items-center shadow-md" >
             <Link href='/main' className="font-bold text-sky-900 text-lg">codpet</Link>
-            <div className="flex bg-zinc-100 items-center text-gray-600 px-3 py-1 rounded-full">
-                <input type="text" placeholder='pesquisar' className="bg-zinc-100 focus-visible:outline-none"/>
+            <div className="flex bg-zinc-100 items-center text-gray-600 px-3 py-1 rounded-full relative">
+                <input type="text" placeholder='pesquisar' className="bg-zinc-100 focus-visible:outline-none" onChange={(e)=> setSearch(e.target.value)} value={search ? search : ''}/>
+            
                 <FaSearch/>
+                {search &&
+                <div className="absolute flex flex-col bg-white p-4 shadow-md rounded-md gap-2 border-t-3 whitespace-nowrap right-[-35px]">
+                    {data?.map((users: IUser, id:number)=>{
+                        return (
+                            <Link href='' key={id}>
+                                <img src={users? users.userImg: 'https://www.digitary.net/wp-content/uploads/2021/07/Generic-Profile-Image.png'} 
+                                alt="imagem do perfil" 
+                                className="u-8 h-8 rounded-full" />
+                                    <span className="font-bold">{users?.username}</span>  
+                                 </Link>
+                        )
+                    })}
+
+
+                </div>}
             </div>
             <div className="flex gap-2 items-center text-gray-600">
                 <div className="flex gap-3 ">
