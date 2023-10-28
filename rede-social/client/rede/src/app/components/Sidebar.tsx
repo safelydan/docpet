809c5e544d7c048c3b8245a89e5c515b1269a968
daniel araujo <daniellealuau@gmail.com>
2023-10-28 12:54:25 -0300

    consertei a função seguir

--- rede-social/client/rede/src/app/components/Sidebar.tsx
+++ rede-social/client/rede/src/app/components/Sidebar.tsx
@@ -28,7 +28,6 @@ function Sidebar() {
                 <Link href='' className="flex gap-3"> <FaUserFriends className='w-6 h-6'/>amigos</Link>
                 <Link href='' className="flex gap-3"> <FaAlignLeft className='w-6 h-6' />feed</Link>
                 <Link href='' className="flex gap-3"> <FaStore className='w-6 h-6' />store</Link>
-                {/* funcionar igual o salvos */}
                 <Link href='' className="flex gap-3" > <FaHeart className='w-6 h-6' />favoritos</Link> 
                 <Link href='' className="flex gap-3" > <FaFlag className='w-6 h-6' />paginas</Link>
                 <Link href='' className="flex gap-3" > <FaCalendar className='w-6 h-6' />eventos</Link>
