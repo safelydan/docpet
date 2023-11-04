b334ec8902e01cb7dd520f8018c8b6578afadb35
daniel araujo <daniellealuau@gmail.com>
2023-11-04 12:31:37 -0300

    rotas

--- rede/api/routes/user.js
+++ rede/api/routes/user.js
@@ -5,7 +5,8 @@ import { checkToken } from '../middleware/tokenVallidation.js'
 const router = express.Router();
 
 router.get('/get-user', getUser);
-router.put('/update-user', checkToken, updateUser, deleteUser); // so faz o update user quando o token for valido
+router.put('/update-user', checkToken, updateUser); // so faz o update user quando o token for valido
+router.delete('/delete-user', checkToken, deleteUser) 
 
 export default router
 
