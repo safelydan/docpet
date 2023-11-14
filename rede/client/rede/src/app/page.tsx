0581aeaa5f9a004eab47239521ba5381aa6784eb
daniel araujo <daniellealuau@gmail.com>
2023-11-14 14:55:50 -0300

    consertado o erro do location

--- rede/client/rede/src/app/page.tsx
+++ rede/client/rede/src/app/page.tsx
@@ -1,14 +1,20 @@
 "use client"
 
-import { useRouter } from 'next/navigation'
+import { useEffect } from 'react';
+import { useRouter } from 'next/router'; // Use 'next/router' em vez de 'next/navigation'
 
 export default function Home() {
+  const router = useRouter();
 
-  const router = useRouter()
-  router.push('/main')
+  useEffect(() => {
+    // Realiza a navegação quando o componente é montado
+    router.push('/main');
+  }, [router]); // Inclua 'router' no array de dependências para corrigir quaisquer avisos de dependência ausente
 
   return (
     <>
+      <p>Redirecionando...</p>
+      {/* Você pode incluir outros conteúdos aqui */}
     </>
-  )
+  );
 }
