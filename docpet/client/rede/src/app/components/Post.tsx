77188158747a14c5f6962741bc81c4d65546eb52
daniel <daniellealuau@gmail.com>
2023-12-06 15:40:05 -0300

    botao de encontrado/perdido adicionado

--- docpet/client/rede/src/app/components/Post.tsx
+++ docpet/client/rede/src/app/components/Post.tsx
@@ -23,6 +23,7 @@ function Post(props:{post: IPost}) {
     const [showComments, setShowComments] = useState(false)
     const [liked, setLiked] = useState(false)
     const [showLikes, setShowLikes] = useState(false)
+    const [status, setStatus] = useState('available');
     const queryClient = useQueryClient()
     
     // Likes QUery
@@ -106,23 +107,37 @@ function Post(props:{post: IPost}) {
         commentMutation.mutate({ comment_desc, comment_user_id: user?.id, post_id: id });
         setComment_desc('')
     }
+
+    const toggleStatus = async () => {
+      const newStatus = status === 'available' ? 'sold' : 'available';
+      await makeRequest.put(`posts/${id}`, { status: newStatus });
+      setStatus(newStatus);
+    };
     
     return (
 <div className="w-full bg-white rounded-lg p-4 shadow-md min-h-[300px]">
   <div className="overflow-hidden"> {/* Adicionado div com overflow-hidden */}
-    <header className="flex gap-2 pb-4 border-b items-center">
-      <Link href={'/profile?id=' + userId}>
-        <img
-          className="w-8 h-8 rounded-full"
-          src={userImg ? userImg : "https://www.digitary.net/wp-content/uploads/2021/07/Generic-Profile-Image.png"}
-          alt="imagem do usuário que fez o post"
-        />
-        <div className="flex flex-col">
-          <span className="font-semibold">{username}</span>
-          <span className="text-xs">{moment(created_at).fromNow().charAt(0).toUpperCase() + moment(created_at).fromNow().slice(1)}</span>
-        </div>
-      </Link>
-    </header>
+  <header className="flex gap-2 pb-4 border-b items-center">
+  <Link href={'/profile?id=' + userId}>
+    <img
+      className="w-8 h-8 rounded-full"
+      src={userImg ? userImg : "https://www.digitary.net/wp-content/uploads/2021/07/Generic-Profile-Image.png"}
+      alt="imagem do usuário que fez o post"
+    />
+    <div className="flex flex-col">
+      <span className="font-semibold">{username}</span>
+      <span className="text-xs">{moment(created_at).fromNow().charAt(0).toUpperCase() + moment(created_at).fromNow().slice(1)}</span>
+    </div>
+  </Link>
+
+  <button
+    onClick={toggleStatus}
+    className="bg-blue-500 text-white px-4 py-2 rounded-full hover:bg-blue-600 focus:outline-none ml-auto"
+  >
+    {status === 'available' ? 'Encontrado' : 'Perdido'}
+  </button>
+</header>
+
 
     {post_desc && (
       <div className="py-4 w-full">
