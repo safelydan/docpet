9e74b2d6a4ccb44ba0b17ad54cc5f5586c2db14d
safelydan <daniellealuau@gmail.com>
2024-12-27 23:28:45 -0300

    olhinho nos campos de senha

--- docpet/client/rede/src/app/components/AuthInput.tsx
+++ docpet/client/rede/src/app/components/AuthInput.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
 // Interface para as propriedades do componente AuthInput
 interface AuthInputProps {
   newState: (state: string) => void; // Função para atualizar o estado no componente pai
@@ -5,19 +8,32 @@ interface AuthInputProps {
   isPassword?: boolean; // Indica se o campo é para senha (opcional, padrão é falso)
 }
 
-function AuthInput(props: AuthInputProps) {
+function AuthInput({ newState, label, isPassword = false }: AuthInputProps) {
+  const [showPassword, setShowPassword] = useState(false); // Controle da visibilidade da senha
+
   return (
-    <div className="flex flex-col justify-between items-start">
-      <label className="text-lg font-medium text-gray-700">{props.label}</label>
-      <input
-        type={props.isPassword ? "password" : "text"}
-        onChange={(e) => props.newState(e.currentTarget.value)}
-        className="          border border-gray-300 
-          focus:ring-2 focus:ring-blue-500 focus:outline-none
-          rounded-lg p-3 w-full 
-          transition duration-300 ease-in-out 
-          hover:border-gray-500"
-      />
+    <div className="flex flex-col justify-between items-start my-4">
+      <label className="text-lg font-medium text-gray-700">{label}</label>
+      <div className="relative w-full">
+        <input
+          type={isPassword && !showPassword ? "password" : "text"} // Alterna entre "password" e "text"
+          onChange={(e) => newState(e.currentTarget.value)}
+          className="border border-gray-300 
+            focus:ring-2 focus:ring-blue-500 focus:outline-none
+            rounded-lg p-3 w-full 
+            transition duration-300 ease-in-out 
+            hover:border-gray-500"
+        />
+        {isPassword && (
+          <button
+            type="button"
+            onClick={() => setShowPassword(!showPassword)} // Alterna o estado de visibilidade da senha
+            className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-600 hover:text-gray-800"
+          >
+            {showPassword ? <FaEyeSlash className="text-xl" /> : <FaEye />}{" "}
+          </button>
+        )}
+      </div>
     </div>
   );
 }
